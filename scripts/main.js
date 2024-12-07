85ce3fc1739f13fdf3dccb25ba982902df957e31
Edwxn <edwinpena0306@gmail.com>
2024-12-07 11:38:14 -0400

    arreglo: style.css arreglo: index.html creación: main.js

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+function createTask() {
+    const inputValue = input.value.trim();
+
+    if(inputValue !== '') {
+        const newTask = document.createElement('div');
+
+        newTask.className = 'tasks active blur-in';
+        newTask.innerText = inputValue;
+
+        setTimeout(function() {
+            output.appendChild(newTask);
+        }, 100);
+
+        input.value = '';
+
+        function delTask() {
+            if(newTask.style.textDecoration == '') {
+                newTask.style.textDecoration = 'line-through';
+
+                newTask.classList.add('blur-out');
+
+                setTimeout(function() {
+                    newTask.remove();
+                }, 400);
+            }
+    
+            else {
+                newTask.style.textDecoration = '';
+            }
+        }
+
+        newTask.addEventListener('click', delTask);
+    }
+}
+
+btn.addEventListener('click', createTask);
